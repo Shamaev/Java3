67746a4fb387ed8c38ca91d91076a63935408da9
Shamaev <79644935622@yandex.ru>
2019-11-10 18:06:37 +0300

    Сделал матрицу. С тестом не смог разобраться....

--- java3_lesson1/src/test/java/TestTask2.java
+++ java3_lesson1/src/test/java/TestTask2.java
@@ -1,12 +1,17 @@
 import lesson6.Task2;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestTask2 {
+    private Task2 task2;
+    @Before
+    public void init () {
+        task2 = new Task2();
+    }
 
     @Test
     public void test1() {
-        Task2 task2 = new Task2();
         int[] arr = {1, 2, 4, 4, 2, 3, 4, 1, 7};
         int[] arr2 = {1,7};
         Assert.assertArrayEquals(arr2, task2.arrFour(arr));
@@ -14,7 +19,6 @@ public class TestTask2 {
 
     @Test
     public void test2() {
-        Task2 task2 = new Task2();
         int[] arr = {1, 2, 3, 4, 5, 4, 3, 2, 1};
         int[] arr2 = {3, 2, 1};
         Assert.assertArrayEquals(arr2, task2.arrFour(arr));
@@ -22,7 +26,6 @@ public class TestTask2 {
 
     @Test
     public void test3() {
-        Task2 task2 = new Task2();
         int[] arr = {4, 4, 4, 4, 4, 4, 4, 4, 7};
         int[] arr2 = {7};
         Assert.assertArrayEquals(arr2, task2.arrFour(arr));
@@ -30,7 +33,6 @@ public class TestTask2 {
 
     @Test
     public void test4() {
-        Task2 task2 = new Task2();
         int[] arr = {1, 2, 2, 3, 1, 7};
         int[] arr2 = {};
         Assert.assertArrayEquals(arr2, task2.arrFour(arr));
